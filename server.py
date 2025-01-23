b90e1466eda312566928c29683ad5ed9dd8300ae
Lisabessa <lisabessa.neyz@gmail.com>
2025-01-24 00:39:04 +0700

    Improve the text

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 import socket
 
 def run_echo_server(host, port):
-	print(f"Сервер запущен на хосте {host}, используя порт {port}")
+	print(f"Сервер запущен для хоста {host}, используя порт {port}")
 	sock = socket.socket()
 	sock.bind((host, port))
 	sock.listen(1)
